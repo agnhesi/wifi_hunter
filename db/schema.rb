9a7536d8f48284860f8ffc0bb0c8843006b867ea
Andressa Agnhesi <aramosagnhesi@gmail.com>
2012-09-08 16:27:17 -0300

    Adicionando o Name da Prey no projeto

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120908154426) do
+ActiveRecord::Schema.define(:version => 20120908192032) do
 
   create_table "preys", :force => true do |t|
     t.string   "person_name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(:version => 20120908154426) do
     t.string   "place"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.string   "name"
   end
 
 end
